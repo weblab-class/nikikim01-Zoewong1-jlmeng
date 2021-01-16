c5d503e40548cb3f0430e9c776b1c503b2058e60
jlmeng <jlmeng@mit.edu>
2021-01-15 16:17:56 -0800

    created CreateEntry page

---
+++ client/src/components/pages/CreateEntry.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class CreateEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  // called when the "Feed" component "mounts", i.e.
+  // when it shows up on screen
+  componentDidMount() {
+    document.title = "Create a New Entry";
+  }
+
+
+
+  render() {
+    
+
+  }
+}
+
+export default CreateEntry;
