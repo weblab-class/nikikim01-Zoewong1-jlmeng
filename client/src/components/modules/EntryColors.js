b4e1e54973b084c88208a14f0b8a34145cdad7f3
jlmeng <jlmeng@mit.edu>
2021-01-15 20:18:09 -0800

    idek circles not workin

--- client/src/components/modules/EntryColors.js
+++ client/src/components/modules/EntryColors.js
@@ -6,6 +6,7 @@ class EntryColors extends Component {
         super(props);
         this.state = {
             selectedColor: null,
+            circles: [],
         }
     }
 
@@ -13,21 +14,33 @@ class EntryColors extends Component {
 
     }
 
+    colorClicked = (color) =>{
+        this.setState({selectedColor: color});
+        console.log("color changed");
+    }
+
     render() {
         //for each color in colors, add a circle of that color to the line
         const colors = ["#B8D4FF", "#B9D99C", "#CAB8FF", "#D99C9C",
         "#F5CCEA", "#F9D142", "#F8E963"];
 
-        let circles = [];
+        let tempcircles = [];
         
         for (let i = 0; i < colors.length; i++) {
             let color = colors[i];
-            circles.push(<Circle key={i+color} bgColor={color}/>);
+            let selected = false;
+            if (color === this.state.selectedColor) {
+                console.log(color);
+                let selected = true;
+            }
+            console.log(color);
+    
+           tempcircles = tempcircles.concat(<Circle key={i+color} bgColor={color} selected={selected} onClick={() => console.log("hello")}/>);
         }
 
         return (
             <div>
-                {circles}
+                {this.state.circles}
             </div>
         )
     
