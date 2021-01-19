586a183e5ec877f0136e11bfa64d2a02f87f8695
Niki Kim <nicolegkim@gmail.com>
2021-01-19 13:37:07 -0800

    fix log in case

--- client/src/components/pages/Locked.js
+++ client/src/components/pages/Locked.js
@@ -26,7 +26,7 @@ class Locked extends Component {
                 <div className="Locked-container">
                     <div className="Locked-welcome u-flex">
                         <Link to="/" className="Locked-logo u-flex"><img src={Welcome} height="200"/></Link>
-                        <h1 className="Locked-header">Please Log In to access our platform!</h1>
+                        <h1 className="Locked-header">Please log in to access our platform!</h1>
                     </div>
                 
                 
