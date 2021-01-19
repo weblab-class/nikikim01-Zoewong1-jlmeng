69d2f8db26db76921473790e7f5e6e9b715b015a
Niki Kim <nicolegkim@gmail.com>
2021-01-18 17:19:34 -0800

    New Month dropdown

---
+++ client/src/components/modules/MonthsDropdown.js
@@ -0,0 +1,72 @@
+import React, { useState, Component } from "react";
+import Select from "react-select";
+
+
+const months = [
+    {
+      value: "Month1",
+      label: 'January',
+    },{
+      value: "Month2",
+      label: 'February',
+    },{
+      value: "Month3",
+      label: 'March',
+    },{
+      value: "Month4",
+      label: 'April',
+    },{
+      value: "Month5",
+      label: 'May',
+    },{
+      value: "Month6",
+      label: 'June',
+    },{
+      value: "Month7",
+      label: 'July',
+    },{
+      value: "Month8",
+      label: 'August',
+    },{
+      value: "Month9",
+      label: 'September',
+    },{
+      value: "Month10",
+      label: 'October',
+    },{
+      value: "Month11",
+      label: 'November',
+    },{
+      value: "Month12",
+      label: 'December',
+    },
+  ];
+
+
+class MonthsDropdown extends Component {
+    state = {
+        selectedOption: null,
+    };
+
+    handleChange = selectedOption => {
+        this.setState({ selectedOption });
+        console.log("Option selected:", selectedOption);
+    };
+
+    render() {
+        const { selectedOption } = this.state;
+
+        return (
+            <Select
+                className = "MonthsDropdown-button"
+                placeholder="Month"
+                value={selectedOption}
+                onChange={this.handleChange}
+                options={months}
+                />
+        );
+    }
+
+}
+
+export default MonthsDropdown;
