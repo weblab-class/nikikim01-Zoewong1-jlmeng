a6dcaba3aa725bf569cfe44d406e29d816bcd6b8
Niki Kim <nicolegkim@gmail.com>
2021-01-26 03:23:32 -0800

    prelim mood tracker css

--- client/src/components/pages/MoodTracker.js
+++ client/src/components/pages/MoodTracker.js
@@ -15,28 +15,28 @@ class MoodTracker extends Component {
                 <div className="MoodTracker-easel">
                     <div className="MoodTracker-canvas">
                     <div className="MoodTracker-Hearts">
-                        <Link to="/"><img className="MoodTracker-Heart" src={'https://storage.googleapis.com/tagheart/heart_000000.svg'} 
-                                height="10%" style={{transform: "rotate(5deg"}}/></Link>
-                        <Link to="/NewEntry"><img className="MoodTracker-Heart" src={'https://storage.googleapis.com/tagheart/heart_0BB5FF.svg'} 
-                                height="10%" style={{transform: "rotate(-9deg"}}/></Link>
-                        <Link to="/"><img className="MoodTracker-Heart" src={'https://storage.googleapis.com/tagheart/heart_54c452.svg'} 
-                                height="10%" style={{transform: "rotate(8deg"}}/></Link>
-                        <Link to="/"><img className="MoodTracker-Heart" src={'https://storage.googleapis.com/tagheart/heart_E35B5B.svg'} 
-                                height="10%" style={{transform: "rotate(-9deg"}} /></Link>
-                        <Link to="/NewEntry"><img className="MoodTracker-Heart" src={'https://storage.googleapis.com/tagheart/heart_717D7E.svg'} 
-                                height="10%" style={{transform: "rotate(4deg"}}/></Link>
-                        <Link to="/"><img className="MoodTracker-Heart" src={'https://storage.googleapis.com/tagheart/heart_965AEA.svg'} 
-                                height="10%" style={{transform: "rotate(-12deg"}}/></Link>
-                        <Link to="/NewEntry"><img className="MoodTracker-Heart" src={'https://storage.googleapis.com/tagheart/heart_9a6a44.svg'} 
-                                height="10%" style={{transform: "rotate(1deg"}}/></Link>
-                        <Link to="/NewEntry"><img className="MoodTracker-Heart" src={'https://storage.googleapis.com/tagheart/heart_F173D2.svg'} 
-                                height="10%" style={{transform: "rotate(6deg"}} /></Link>
-                        <Link to="/"><img className="MoodTracker-Heart" src={'https://storage.googleapis.com/tagheart/heart_FEC085.svg'} 
-                                height="10%" style={{transform: "rotate(-7deg"}} /></Link>
-                        <Link to="/NewEntry"><img className="MoodTracker-Heart" src={'https://storage.googleapis.com/tagheart/heart_FFD300.svg'} 
-                                height="10%" style={{transform: "rotate(7deg"}}/></Link>
-                        <Link to="/"><img className="MoodTracker-Heart" src={'https://storage.googleapis.com/tagheart/heart_6BA0FC.svg'} 
-                                height="10%" style={{transform: "rotate(-3deg"}}/></Link>
+                        <Link to="/"><img className="MoodTracker-Heart blackHeart" src={'https://storage.googleapis.com/tagheart/heart_000000.svg'} 
+                                height="40%"/></Link>
+                        <Link to="/NewEntry"><img className="MoodTracker-Heart happyBlueHeart" src={'https://storage.googleapis.com/tagheart/heart_0BB5FF.svg'} 
+                                height="40%"/></Link>
+                        <Link to="/"><img className="MoodTracker-Heart greenHeart" src={'https://storage.googleapis.com/tagheart/heart_54c452.svg'} 
+                                height="80%"/></Link>
+                        <Link to="/"><img className="MoodTracker-Heart redHeart" src={'https://storage.googleapis.com/tagheart/heart_E35B5B.svg'} 
+                                height="40%"/></Link>
+                        <Link to="/NewEntry"><img className="MoodTracker-Heart greyHeart" src={'https://storage.googleapis.com/tagheart/heart_717D7E.svg'} 
+                                height="40%"/></Link>
+                        <Link to="/"><img className="MoodTracker-Heart purpleHeart" src={'https://storage.googleapis.com/tagheart/heart_965AEA.svg'} 
+                                height="40%"/></Link>
+                        <Link to="/NewEntry"><img className="MoodTracker-Heart brownHeart" src={'https://storage.googleapis.com/tagheart/heart_9a6a44.svg'} 
+                                height="50%"/></Link>
+                        <Link to="/NewEntry"><img className="MoodTracker-Heart pinkHeart" src={'https://storage.googleapis.com/tagheart/heart_F173D2.svg'} 
+                                height="40%"/></Link>
+                        <Link to="/"><img className="MoodTracker-Heart orangeHeart" src={'https://storage.googleapis.com/tagheart/heart_FEC085.svg'} 
+                                height="60%" /></Link>
+                        <Link to="/NewEntry"><img className="MoodTracker-Heart yellowHeart" src={'https://storage.googleapis.com/tagheart/heart_FFD300.svg'} 
+                                height="90%"/></Link>
+                        <Link to="/"><img className="MoodTracker-Heart sadBlueHeart" src={'https://storage.googleapis.com/tagheart/heart_6BA0FC.svg'} 
+                                height="40%"/></Link>
                     </div>
                     </div>
                     
